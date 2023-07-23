f3812890c690c16b7e9410507f7feabba47e23a3
TarangKh <tarangkharkar4448@gmail.com>
2023-07-24 00:01:07 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+import pickle
+import numpy as np
+from flask import Flask,request, url_for, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+model=pickle.load(open('modell.pkl','rb'))
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[float(x) for x in request.form.values()]
+    final=[int_features]
+    print(int_features) 
+
+    print(final)
+    prediction=model.predict(final)
+    output='{0}'.format(prediction)
+
+    if output<str(1):
+        return render_template('index.html',pred='Your Country has good Mental Health.\n DALY is {}'.format(output),bhai=" Itni Khushi")
+    else:
+        return render_template('index.html',pred='Your Country has not Good mental Health.\n  DALY is {}'.format(output),bhai=" Utha Le Re BABA")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True, port = 8000)
